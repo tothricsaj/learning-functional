3fb43046fa0ea72c52b78d0973c9e4fb36fa8664
tothricsaj <tothricsaj@gmail.com>
2020-05-12 22:12:37 +0200

    unless function

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -1,4 +1,4 @@
-import { forEach, forEachObject } from '../lib/es8-functional.js'
+import { forEach, forEachObject, unless } from '../lib/es8-functional.js'
 
 let array = [1,2,3,4]
 forEach(array, (data) => console.log(data))
@@ -7,3 +7,7 @@ console.log(array)
 
 let obj = {a: 1, b: 2}
 forEachObject(obj, (k, v) => console.log(`${k}: ${v}`))
+
+forEach([1,2,3,4,5,6,7], (number) => {
+    unless(number%2, () => console.log(`${number} is even!`))
+})
