6d9382c94c6a8ce16655d7a74974cbdad631bd73
tothricsaj <tothricsaj@gmail.com>
2020-05-12 22:04:07 +0200

    foreachobject is because of object traversing

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -1,6 +1,9 @@
-import forEach from '../lib/es8-functional.js'
+import { forEach, forEachObject } from '../lib/es8-functional.js'
 
 let array = [1,2,3,4]
 forEach(array, (data) => console.log(data))
 forEach(array, (data) => data++)
 console.log(array)
+
+let obj = {a: 1, b: 2}
+forEachObject(obj, (k, v) => console.log(`${k}: ${v}`))
