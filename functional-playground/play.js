866bead779463d88196c2fdf44c039960ad504a7
tothricsaj <tothricsaj@gmail.com>
2020-05-14 18:53:31 +0200

    every function implementation

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -1,4 +1,4 @@
-import { forEach, forEachObject, unless } from '../lib/es8-functional.js'
+import { forEach, forEachObject, unless, every } from '../lib/es8-functional.js'
 
 let array = [1,2,3,4]
 forEach(array, (data) => console.log(data))
@@ -11,3 +11,8 @@ forEachObject(obj, (k, v) => console.log(`${k}: ${v}`))
 forEach([1,2,3,4,5,6,7], (number) => {
     unless(number%2, () => console.log(`${number} is even!`))
 })
+
+console.log('/////// every function ///////')
+
+console.log(every([NaN, NaN, NaN], isNaN))
+console.log(every(['foo', 'foo', 'fooBAr'], (data) => data==='foo' || data==='fooBar' ? true : false))
