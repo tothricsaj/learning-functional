e93e2ac9f9c566fa27d4608612bc6f404674b33a
tothricsaj <tothricsaj@gmail.com>
2020-05-15 21:23:42 +0200

    unary fucniton

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -1,4 +1,4 @@
-import { forEach, forEachObject, unless, every, some, tap } from '../lib/es8-functional.js'
+import { forEach, forEachObject, unless, every, some, tap, unary } from '../lib/es8-functional.js'
 
 let array = [1,2,3,4]
 forEach(array, (data) => console.log(data))
@@ -23,3 +23,6 @@ console.log(some([2,1,'foo'], (data) => data==='foo' || data==='fooBar' ? true :
 
 console.log('/////// tap function ///////')
 tap('fun')((it) => console.log('value is ', it))
+console.log('/////// unary function ///////')
+console.log(['1','2','3'].map(unary(parseInt)))
+
