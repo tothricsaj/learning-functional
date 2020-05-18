e2fac523e4dda15107308a3f90b63064e9ed769d
tothricsaj <tothricsaj@gmail.com>
2020-05-18 19:27:59 +0200

    lib is reachable from global

---
+++ libApi.js
@@ -0,0 +1,13 @@
+import { forEach, forEachObject, unless, every, some, tap, unary, once, memoize } from './lib/es8-functional.js'
+
+window.myLib = {
+    forEachObject: forEachObject,
+    forEach: forEach,
+    unless: unless,
+    every: every,
+    some: some,
+    tap: tap,
+    unary: unary,
+    once: once,
+    memoize: memoize,
+}
