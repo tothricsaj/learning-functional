bea19f2d057a25a5d2ee134aa5d81fe3e1e39a74
tothricsaj <tothricsaj@gmail.com>
2020-06-02 22:12:15 +0200

    createStore.....:smile:

--- redux.js
+++ redux.js
@@ -1,3 +1,5 @@
+import { reduce } from "./lib/es8-functional"
+
 let initialStat = {counter: 0}
 function reducer(state, action) {
     if(action.type = 'INCREMENT') state = Object.assign({}, state, {counter: state.counter + 1})
@@ -10,4 +12,38 @@ function incrementCounter() {
     store.dispatch({
         type: 'INCREMENT'
     })
+}
+
+function createStore(reducer, preloadedState) {
+    let currentReducer = reduce
+    let currentState = preloadedState
+    let currentListeners = []
+    let nextListeners = currentListeners
+
+    function getState() {
+        return currentState
+    }
+
+    function dispatch(action) {
+        currentState = currentReducer(currentState, action)
+
+        const listeners = currentListeners = nextListeners
+
+        for(let i=0; i<listeners.length; i++) {
+            const listener = listener[i]
+            listener()
+        }
+
+        return action
+    }
+
+    function subscribe(listener) {
+        nextListeners.push(listener)
+    }
+
+    return {
+        getState,
+        dispatch,
+        subscribe
+    }
 }
