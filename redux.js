e29ed98c53a95eeff8d346e7ec36e41854d31f74
tothricsaj <tothricsaj@gmail.com>
2020-06-02 22:02:59 +0200

    increment counter and click event on button

--- redux.js
+++ redux.js
@@ -3,3 +3,11 @@ function reducer(state, action) {
     if(action.type = 'INCREMENT') state = Object.assign({}, state, {counter: state.counter + 1})
     return state
 }
+
+document.getElementById('button').addEventListener('click', () => incrementCounter())
+
+function incrementCounter() {
+    store.dispatch({
+        type: 'INCREMENT'
+    })
+}
