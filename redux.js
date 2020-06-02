354aa4edda4ceb9d2f9fdcde50370198fb4178b3
tothricsaj <tothricsaj@gmail.com>
2020-06-02 22:00:04 +0200

    reducer function ----- so simple

--- redux.js
+++ redux.js
@@ -1,1 +1,5 @@
 let initialStat = {counter: 0}
+function reducer(state, action) {
+    if(action.type = 'INCREMENT') state = Object.assign({}, state, {counter: state.counter + 1})
+    return state
+}
